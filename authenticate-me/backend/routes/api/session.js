7b7e34c13d54eb2a47c730b447e956a0015eba43
Ryan Goggin <ryangoggin1@gmail.com>
2023-01-19 17:18:16 -0500

    Add User logout backend endpoint

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -28,6 +28,15 @@ router.post(
         user: user
       });
     }
-  );
+);
+
+// Phase 4: Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+);
 
 module.exports = router;
