f4fffd871436b310ae2d31c4e2551911c9e3499a
Ryan Goggin <ryangoggin1@gmail.com>
2023-01-19 14:10:11 -0500

    Add error-handling middleware for common errors and debugging

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -6,7 +6,12 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const routes = require('./routes'); // Import routes from routes folder
+
+// Phase 1: Import routes from routes folder
+const routes = require('./routes');
+
+// Phase 2:
+const { ValidationError } = require('sequelize');
 
 // isProduction is true if environment is in production in the backend/config/index.js
 const { environment } = require('./config');
@@ -46,6 +51,38 @@ if (!isProduction) {
     })
   );
 
-app.use(routes); // Connect all the routes
+// Phase 1: Connect all the routes
+app.use(routes);
+
+// Phase 2: Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+// Phase 2: Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+});
+
+// Phase 2: Error formatter (last middleware fxn)
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app; // Export app for use in other files
