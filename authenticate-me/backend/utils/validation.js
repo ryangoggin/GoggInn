1aeb757d2a24d8073e73e80672450102f3647258
Ryan Goggin <ryangoggin1@gmail.com>
2023-01-19 18:01:33 -0500

    added middelware to handle login input validation errors

---
+++ authenticate-me/backend/utils/validation.js
@@ -0,0 +1,25 @@
+// Phase 5:
+const { validationResult } = require('express-validator');
+
+// Phase 5: middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = validationErrors
+      .array()
+      .map((error) => `${error.msg}`);
+
+    const err = Error('Bad request.');
+    err.errors = errors;
+    err.status = 400;
+    err.title = 'Bad request.';
+    next(err);
+  }
+  next();
+};
+
+module.exports = {
+  handleValidationErrors
+};
