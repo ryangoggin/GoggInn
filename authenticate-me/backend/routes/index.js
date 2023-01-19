d8b7dfed68834e116315de5e22df2eea60616a60
Ryan Goggin <ryangoggin1@gmail.com>
2023-01-19 14:00:25 -0500

    Add an API router

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,6 +1,7 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api'); //import from index.js in api folder
 
 // Add a XSRF-TOKEN cookie --> should not be available in production
 router.get("/api/csrf/restore", (req, res) => {
@@ -10,5 +11,8 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
+
+// API router
+router.use('/api', apiRouter);
 
 module.exports = router;
